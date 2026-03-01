9cf7599d6b25cb507a9943878e7a86ba6d4efedd
John19110 <2022170119@cis.asu.edu.eg>
2026-03-01 13:31:39 +0200

    daily updates

---
+++ SunDaySchools.BLL/DTOS/AttendanceSessionAddDTO.cs
@@ -0,0 +1,29 @@
+﻿using SunDaySchools.DAL.Models;
+using SunDaySchools.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SunDaySchools.BLL.DTOS
+{
+    public class AttendanceSessionAddDTO
+    {
+        public int ClassroomId { get; set; }
+
+        // The day the weekly meeting happened
+        public DateOnly SessionDate { get; set; }
+
+        // Who took attendance
+        public int? TakenByServantId { get; set; }
+
+        public string? Notes { get; set; }
+
+        public DateTime CreatedAtUtc { get; set; } = DateTime.UtcNow;
+
+        // All child records for this session
+        public List<AttendanceRecord> Records { get; set; } = new();
+
+    }
+}
---
+++ SunDaySchools.BLL/DTOS/AttendanceSessionUpdateDTO.cs
@@ -0,0 +1,31 @@
+﻿using SunDaySchools.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SunDaySchools.BLL.DTOS
+{
+    public class AttendanceSessionUpdateDTO
+    {
+        public int Id { get; set; }
+
+        public int ClassroomId { get; set; }
+
+        // The day the weekly meeting happened
+        public DateOnly SessionDate { get; set; }
+
+        // Who took attendance
+        public int? TakenByServantId { get; set; }
+
+        public string? Notes { get; set; }
+
+        public DateTime CreatedAtUtc { get; set; } = DateTime.UtcNow;
+
+        // All child records for this session
+        public List<AttendanceRecord> Records { get; set; } = new();
+
+
+    }
+}
