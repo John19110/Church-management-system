05d8916cbd7bb8275f82ea9a593ee63d65c096ef
John19110 <2022170119@cis.asu.edu.eg>
2026-03-04 20:26:05 +0200

    daily updates

--- SunDaySchools.BLL/DTOS/ChildReadDTO.cs
+++ SunDaySchools.BLL/DTOS/ChildReadDTO.cs
@@ -18,7 +18,7 @@ namespace SunDaySchools.BLL.DTOS
         public string? Gender { get; set; }
         public DateOnly DateOfBirth { get; set; }
         public DateOnly JoiningDate { get; set; }
-        public DateOnly LastAttendanceDate { get; set; }
+        public DateTime LastAttendanceDate { get; set; }
         public DateOnly? SpiritualDateOfBirth { get; set; }
         public bool? IsDisciplineChild { get; set; }
         public int? TotalNumberOfDaysAttended { get; set; }
