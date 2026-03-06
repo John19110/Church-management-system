b16c2712330bfbfcde3d4f4e0568046c791fd492
John19110 <2022170119@cis.asu.edu.eg>
2026-03-06 20:46:55 +0200

    daily updates

--- SunDaySchools.BLL/DTOS/ServantReadDTO.cs
+++ SunDaySchools.BLL/DTOS/ServantReadDTO.cs
@@ -14,12 +14,9 @@ namespace SunDaySchools.BLL.DTOS
         public string? ImageFileName { get; set; }   // e.g. "c3c2... .jpg"
         public string? ImageUrl { get; set; }
         public string? Name { get; set; }
-        public string? password { get; set; }
         public DateOnly? BirthDate { get; set; }
-
         public DateOnly? JoiningDate { get; set; }
         public string? PhoneNumber { get; set; }
         public int? ClassroomId { get; set; }
-        public Classroom? Classroom { get; set; }
     }
 }
