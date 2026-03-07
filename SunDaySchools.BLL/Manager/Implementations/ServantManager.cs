4776b4c9f1698ce50facb8ac559bab36faeced9a
John19110 <2022170119@cis.asu.edu.eg>
2026-03-08 01:48:30 +0200

    daily updates

--- SunDaySchools.BLL/Manager/Implementations/ServantManager.cs
+++ SunDaySchools.BLL/Manager/Implementations/ServantManager.cs
@@ -1,14 +1,15 @@
 ﻿using AutoMapper;
 using SunDaySchools.BLL.DTOS;
+using SunDaySchools.BLL.Exceptions;
+using SunDaySchools.BLL.Manager.Interfaces;
 using SunDaySchools.DAL.Repository;
+using SunDaySchools.DAL.Repository.Interfaces;
 using SunDaySchools.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using SunDaySchools.DAL.Repository.Interfaces;
-using SunDaySchools.BLL.Manager.Interfaces;
 
 namespace SunDaySchools.BLL.Manager.Implementations
 {
@@ -34,13 +35,18 @@ namespace SunDaySchools.BLL.Manager.Implementations
         {
             _sarventReposatory.Add(_mapper.Map<Servant>(servant));
         }
-     public  void Update(ServantUpdateDTO ServantUpdateDTO)
+        public void Update(ServantUpdateDTO servantUpdateDTO)
         {
-            _sarventReposatory.Update(_mapper.Map(ServantUpdateDTO, _sarventReposatory.GetById(ServantUpdateDTO.Id)));
+            var existing = _sarventReposatory.GetById(servantUpdateDTO.Id);
 
+            if (existing == null)
+                throw new NotFoundException($"Servant with id {servantUpdateDTO.Id} not found.");
+
+            _mapper.Map(servantUpdateDTO, existing);
+            _sarventReposatory.Update(existing);
         }
 
-      public  void Delete(int id)
+        public  void Delete(int id)
         {
 
             _sarventReposatory.Delete(id);
