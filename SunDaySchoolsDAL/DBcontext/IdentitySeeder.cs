c14e1a047cfc9d813a020e34360b3cf3c01131d3
John19110 <2022170119@cis.asu.edu.eg>
2026-03-10 16:19:12 +0200

    dialy updates

---
+++ SunDaySchoolsDAL/DBcontext/IdentitySeeder.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace SunDaySchoolsDAL.DBcontext
+{
+    public static class IdentitySeeder
+    {
+        public static async Task SeedRolesAsync(RoleManager<IdentityRole> roleManager)
+        {
+            string[] roles = { "Admin", "Servant" };
+
+            foreach (var role in roles)
+            {
+                if (!await roleManager.RoleExistsAsync(role))
+                {
+                    await roleManager.CreateAsync(new IdentityRole(role));
+                }
+            }
+        }
+    }
+}
