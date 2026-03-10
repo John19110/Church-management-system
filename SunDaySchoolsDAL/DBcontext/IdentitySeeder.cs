937419267624290bec3be1e180c7e828a5fa4733
John19110 <2022170119@cis.asu.edu.eg>
2026-03-10 16:43:29 +0200

    dialy updates

--- SunDaySchoolsDAL/DBcontext/IdentitySeeder.cs
+++ SunDaySchoolsDAL/DBcontext/IdentitySeeder.cs
@@ -1,10 +1,13 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using SunDaySchoolsDAL.Models;
 
 namespace SunDaySchoolsDAL.DBcontext
 {
     public static class IdentitySeeder
     {
-        public static async Task SeedRolesAsync(RoleManager<IdentityRole> roleManager)
+        public static async Task SeedIdentityAsync(
+            RoleManager<IdentityRole> roleManager,
+            UserManager<ApplicationUser> userManager)
         {
             string[] roles = { "Admin", "Servant" };
 
@@ -12,13 +15,25 @@ namespace SunDaySchoolsDAL.DBcontext
             {
                 if (!await roleManager.RoleExistsAsync(role))
                 {
-                    var result=  await roleManager.CreateAsync(new IdentityRole(role));
+                    var result = await roleManager.CreateAsync(new IdentityRole(role));
+
                     if (!result.Succeeded)
                     {
                         throw new Exception($"Failed to create role {role}");
                     }
                 }
-                
+            }
+
+            var admin = await userManager.FindByNameAsync("john");
+
+            if (admin != null && !await userManager.IsInRoleAsync(admin, "Admin"))
+            {
+                var result = await userManager.AddToRoleAsync(admin, "Admin");
+
+                if (!result.Succeeded)
+                {
+                    throw new Exception("Failed to assign Admin role");
+                }
             }
         }
     }
--- SunDaySchoolsDAL/Models/ApplicationUser.cs
+++ SunDaySchoolsDAL/Models/ApplicationUser.cs
@@ -3,13 +3,24 @@ using SunDaySchools.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
+using System.Security;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 namespace SunDaySchoolsDAL.Models
 {
     public class ApplicationUser : IdentityUser
     {
         public Servant? ServantProfile { get; set; } // navigation only
+                                                     //the ? means that not all the user should be servants they may be admin only 
+
+
+        // Servant table = servant data such as classroom, phone calls, attendance work, etc.
+
+        // Servant role = permission to access servant endpoints/pages
+
+        // Admin role = permission to access admin endpoints/pages
 
     }
 }
