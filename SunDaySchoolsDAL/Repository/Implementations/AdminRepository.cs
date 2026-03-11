6676fa843f1435f2cb08aa1becf6ebe26a44fe6c
John19110 <2022170119@cis.asu.edu.eg>
2026-03-11 07:44:25 +0200

    dialy updates

---
+++ SunDaySchoolsDAL/Repository/Implementations/AdminRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SunDaySchools.DAL.Repository.Implementations
+{
+    public class AdminRepository
+    {
+
+
+
+
+    }
+}
