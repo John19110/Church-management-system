81c208f279f5e1cf5a032fbe2813db3b90de4c19
John19110 <2022170119@cis.asu.edu.eg>
2026-03-11 08:26:53 +0200

    dialy updates

--- SunDaySchoolsDAL/Repository/Interfaces/IAdminRepository.cs
+++ SunDaySchoolsDAL/Repository/Interfaces/IAdminRepository.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SunDaySchools.DAL.Models;
+using SunDaySchools.Models;
 
 namespace SunDaySchools.DAL.Repository.Interfaces
 {
     public  interface IAdminRepository
     {
-        void AssignClassToServant();
+        (Servant,Classroom) AssignClassToServant();
 
         void ApprovServant();
 
