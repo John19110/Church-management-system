ebb8ca6b4f015530dabe4d0401f15d13098a4542
John19110 <2022170119@cis.asu.edu.eg>
2026-03-12 17:55:27 +0200

    dialy updates

--- SunDaySchools.API/Controllers/AdminController.cs
+++ SunDaySchools.API/Controllers/AdminController.cs
@@ -1,17 +1,38 @@
+using Microsoft.AspNetCore.Mvc;
+using SunDaySchools.BLL.DTOS.AccountDtos;
+using SunDaySchools.BLL.Manager.Implementations;
+using SunDaySchools.BLL.Manager.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using SunDaySchools.BLL.DTOS.AccountDtos;
-using SunDaySchools.BLL.Manager.Interfaces;
 namespace SunDaySchools.API.Controllers
 {
     [ApiController]
     [Route("Api/[controller]")]
     public class AdminController : ControllerBase
     {
+
+        private readonly IAdminManager _adminManager;
+        public AdminController(IAdminManager adminmanager)
+        {
+            _adminManager = adminmanager;
+
+        }
+
+        [HttpPut("{ServantId}/{ClassroomId}")]
+
+        public ActionResult AssignClassToServant(int ServantId,int ClassroomId)
+        {
+
+
+            _adminManager.AssignClassToServant(ServantId, ClassroomId);
+            return Ok();
+
+        }
+
+
 
 
 
