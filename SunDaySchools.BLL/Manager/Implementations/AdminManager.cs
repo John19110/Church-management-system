ccd7b74a72b7a7e0f45adfe355fd4d8b9d4b6ab5
John19110 <2022170119@cis.asu.edu.eg>
2026-03-12 17:13:56 +0200

    dialy updates

--- SunDaySchools.BLL/Manager/Implementations/AdminManager.cs
+++ SunDaySchools.BLL/Manager/Implementations/AdminManager.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using SunDaySchools.DAL.Repository.Implementations;
+using SunDaySchools.DAL.Repository.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +10,15 @@ namespace SunDaySchools.BLL.Manager.Implementations
 {
     public class AdminManager
     {
+        private readonly IAdminRepository _adminRepository;
+
+
+        public AdminManager(IAdminRepository adminRepository)
+        {
+            _adminRepository = adminRepository;
+        }
+
+
 
     }
 }
