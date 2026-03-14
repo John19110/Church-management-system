85dee2196604d49c7b254d7b749fa96bf800b39b
John19110 <2022170119@cis.asu.edu.eg>
2026-03-14 21:25:32 +0200

    dialy updates

--- SunDaySchools.API/Controllers/AdminController.cs
+++ SunDaySchools.API/Controllers/AdminController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using SunDaySchools.API.Requests;
 using SunDaySchools.BLL.DTOS.AccountDtos;
 using SunDaySchools.BLL.Manager.Implementations;
 using SunDaySchools.BLL.Manager.Interfaces;
@@ -7,6 +8,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SunDaySchools.API.Mapping;
+using SunDaySchools.API.Requests;
+using SunDaySchools.API.Services.Interfaces;
+using SunDaySchools.BLL.Exceptions;
+using SunDaySchools.BLL.Manager.Interfaces;
+using System.Security.Claims;
 namespace SunDaySchools.API.Controllers
 {
     [ApiController]
@@ -30,12 +38,25 @@ namespace SunDaySchools.API.Controllers
 
         }
 
-
-        public ActionResult AddServant(int ServantId, int ClassroomId)
+        [HttpPost("add-servant{}")]
+        [Consumes("multipart/form-data")]
+        public async Task<IActionResult> AddServant([FromForm] ServantFormRequest form, CancellationToken ct)
         {
-            _adminManager.AssignClassToServant(ServantId, ClassroomId);
-            return Ok();
+            if (!ModelState.IsValid)
+                return ValidationProblem(ModelState);
 
+            var dto = form.ToAddDto();
+
+            if (form.Image is not null && form.Image.Length > 0)
+            {
+                var key = await _fileStorage.SaveImageAsync(form.Image, ct, "servants");
+                dto.ImageFileName = key;
+                dto.ImageUrl = _fileStorage.GetPublicUrl(key);
+            }
+
+            _servantManager.Add(dto);
+
+            return Ok();
         }
 
 
