da898ffdae785b50975a7a5b342e8e17e01ad1ce
John19110 <2022170119@cis.asu.edu.eg>
2026-03-21 13:41:39 +0200

    Daily updates

--- SunDaySchoolsDAL/Repository/Interfaces/IClassroomRepository.cs
+++ SunDaySchoolsDAL/Repository/Interfaces/IClassroomRepository.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using SunDaySchools.Models;
+using SunDaySchoolsDAL.DBcontext;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,7 +11,18 @@ namespace SunDaySchools.DAL.Repository.Interfaces
     public interface IClassroomRepository
     {
 
+        public IQueryable<Classroom> GetAll();
+        public Classroom? GetById(int id);
+        public void Add(Classroom classroom);
+        public void Update(Classroom classroom);
+        public void Delete(int id);
+            
 
-
+        }
     }
-}
+
+
+
+
+
+
